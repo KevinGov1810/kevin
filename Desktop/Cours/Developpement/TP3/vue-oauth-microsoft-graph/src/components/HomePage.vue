3bb0c8c9b623491cee57a4ee467c1c15694dfdf3
Gov <kevin@macbook-pro-de-gov.home>
2021-10-06 18:53:37 +0200

    Ex 7: create BaseButton with primary color

--- Desktop/Cours/Developpement/TP3/vue-oauth-microsoft-graph/src/components/HomePage.vue
+++ Desktop/Cours/Developpement/TP3/vue-oauth-microsoft-graph/src/components/HomePage.vue
@@ -1,3 +1,47 @@
 <template>
-    <h1 style="text-align:center; margin-top:200px"> Here comes the content of the Homepage </h1>
+    <h1> Here comes the content of the Homepage </h1>
+    <div class="centre">
+        <button type ="button1">BaseButton with custom margin </button>
+        <button disabled>BaseButton disabled </button>
+    </div>
 </template>
+
+<style scoped>
+
+h1 {
+    text-align:center;
+    margin-top:200px
+}
+
+button {
+    display: inline-block;
+    background-color: #222;
+    color : lightgreen;
+    border-radius: 10px;
+    text-align: center;
+    font-size: 28px;
+    padding: 20px;
+    width: 500px;
+    -webkit-transition: all 0.5s;
+    -moz-transition: all 0.5s;
+    -o-transition: all 0.5s;
+    transition: all 0.5s;
+    cursor: pointer;
+    margin: 5px;
+}
+
+button:hover {
+    background-color: rgb(221, 231, 231);
+}
+
+button:disabled,
+button[disabled]{
+  border: 1px solid #999999;
+  background-color: #cccccc;
+  color: #666666;
+}
+
+.centre{
+  text-align: center;
+}
+</style>
